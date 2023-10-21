1efecbf037cbc9af2261d3d86bb4a58801282667
Tan Nguyen <tannguyen1220@gmail.com>
2023-10-21 11:57:17 +0700

    visual update

--- components/footer/index.js
+++ components/footer/index.js
@@ -6,8 +6,8 @@ const Footer = () => {
     const currentYear = new Date().getFullYear(); //To get the Current Year
     const theme = useTheme();
     return (
-        <View className=' text-center bg-slate-400 text-white grid-cols-1 min-w-full p-6 ' style={{ backgroundColor: theme.colors.secondary }}>
-            <Text className=' text-white mb-4'>
+        <View className=' text-center bg-slate-400 text-white grid-cols-1 min-w-full pb-6 pt-3' style={{ backgroundColor: theme.colors.secondary }}>
+            <Text className=' text-white pb-4'>
                 App by Nguyen Viet Tan
             </Text>
             <Text className=' text-white'>
